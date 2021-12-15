0a1226757161f57a04b2f6a2f190f3955ba750be
tarkerin <feelthegamesense@gmail.com>
2021-12-15 11:55:57 -0300

    password generator

--- password.py
+++ password.py
@@ -1,9 +1,19 @@
+t =  """___________    ______ ________  _  _____________  __| _/
+\____ \__  \  /  ___//  ___/\ \/ \/ /  _ \_  __ \/ __ | 
+|  |_> > __ \_\___ \ \___ \  \     (  <_> )  | \/ /_/ | 
+|   __(____  /____  >____  >  \/\_/ \____/|__|  \____ | 
+|__|       \/     \/     \/                          \/  By: tarkerin           
+"""
 import random
 import string
-
+print(t)
 x = 0 
 password = ""
-qtd = int(input('Digite a quantidade de caracteres que você deseja na sua senha: '))
+y = input('Você deseja gerar uma senha? S/N ')
+if y == 'S':
+    qtd = int(input('Digite a quantidade de caracteres que você deseja na sua senha: '))
+else:
+    exit()
 
 for x in range(qtd):
     i = random.choice(string.ascii_lowercase + string.digits + string.ascii_uppercase)
